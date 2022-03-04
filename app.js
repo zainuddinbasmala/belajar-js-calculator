f3737abf541226f9298bcf33c31d9a5b3661f226
zainuddinbasmala <zainuddinbasmala@gmail.com>
2022-03-04 10:22:08 +0700

    revisi click tombol

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ let operasi = "";
 for(let i = 0; i < nilai.length; i++){
     nilai[i].addEventListener("click", function(){
         if(nilai[i].innerText === "+"){
-            operasi = nilai[i].innerText;
             angka1 = inputBox.value;
+            operasi = "+"
             inputBox.value = "0";
         } else if(nilai[i].innerText === "=") {
             angka2 = inputBox.value;
             if(operasi === "+") {
                 inputBox.value = parseInt(angka1) + parseInt(angka2)
+            }
+        } else {
+            if(inputBox.value === "0"){
+                inputBox.value = nilai[i].innerText
+            } else {
+                inputBox.value = inputBox.value + nilai[i].innerText
             }
         }
     })
