1c3c6b26d0d557a2c9b97dd30dc04e127d5abf1b
zainuddinbasmala <zainuddinbasmala@gmail.com>
2022-03-04 14:10:24 +0700

    calculator lengkap

--- app.js
+++ app.js
@@ -8,12 +8,33 @@ for(let i = 0; i < nilai.length; i++){
     nilai[i].addEventListener("click", function(){
         if(nilai[i].innerText === "+"){
             angka1 = inputBox.value;
-            operasi = "+"
+            operasi = "+";
+            inputBox.value = "0";
+        } else if(nilai[i].innerText === "-"){
+            angka1 = inputBox.value;
+            operasi = "-";
+            inputBox.value = "0";
+        } else if(nilai[i].innerText === "*"){
+            angka1 = inputBox.value;
+            operasi = "*";
+            inputBox.value = "0";
+        } else if(nilai[i].innerText === "/"){
+            angka1 = inputBox.value;
+            operasi = "/";
             inputBox.value = "0";
         } else if(nilai[i].innerText === "=") {
             angka2 = inputBox.value;
             if(operasi === "+") {
                 inputBox.value = parseInt(angka1) + parseInt(angka2)
+            }
+            if(operasi === "-") {
+                inputBox.value = parseInt(angka1) - parseInt(angka2)
+            }
+            if(operasi === "*") {
+                inputBox.value = parseInt(angka1) * parseInt(angka2)
+            }
+            if (operasi === "/") {
+                inputBox.value = parseInt(angka1) / parseInt(angka2)
             }
         } else {
             if(inputBox.value === "0"){
